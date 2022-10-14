7bfec1f587325426c18baa15350bacd2b4a23556
Szymon Witusiak <szymon.witusiak@gmail.com>
2022-10-14 12:46:19 +0200

    Population initialization

--- GA.py
+++ GA.py
@@ -1,19 +1,24 @@
 tsp_file = open("TSP/berlin52.tsp", "r")
+population_size = 4
 
+#Wczytanie nagłówka
 from head_reader import HeadReader
 HeadReader.read(tsp_file)
 
+#Wczytanie wartości
 from cities_reading import CitiesReading
 cities_list = CitiesReading.read(tsp_file)
 
+#Pomiar jakości rozwiązania
 from road_length import RoadLength
 length = RoadLength.count(cities_list)
 print(length)
 
-import random
-random.shuffle(cities_list)
-
-length = RoadLength.count(cities_list)
-print(length)
+#Inicjalizacja populacji
+from city import city
+population = city.population_initialization(city, cities_list, population_size)
+print(len(population))
+for i in range(population_size):
+    print(RoadLength.count(population[i]))
 
 #print(len(cities_list))
--- city.py
+++ city.py
@@ -9,3 +9,16 @@ class city:
     
     def __repr__(self):
         return "['" + str(self.lp) + "', '" + str(self.x) + "', '" + str(self.y) + "']"
+    
+    def list_randomization(cities_list):
+        import random
+        cities_list_copy = cities_list.copy()
+        random.shuffle(cities_list_copy)
+        return cities_list_copy
+    
+    def population_initialization(self, cities_list, amount):
+        population = []
+        for i in range(amount):
+            population.append(self.list_randomization(cities_list))
+        #print(population)
+        return population
