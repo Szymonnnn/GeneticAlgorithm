bfb975389a4d96f33508850f474254c959e21d48
Szymonnnn <szymon.witusiak@gmail.com>
2022-10-24 23:10:25 +0200

    ruletka

--- selection.py
+++ selection.py
@@ -1,18 +1,25 @@
 import random
 
-def roulette(road_lengths, selection_presure):
+def tournament(population, selection_pressure):
+    contestants = random.sample(range(len(population)), selection_pressure)
+    winner = contestants[0]
+    for contestant in contestants[1:]:
+        if population[contestant] < population[winner]:
+            winner = contestant
+    return population[winner]
+
+def roulette(population, selection_presure):
     #inversion of road lengths
     road_lengths_inverse = []
-    for i in range(len(road_lengths)):
-        road_lengths_inverse.append(1/road_lengths[i].cost)
-
+    for i in population:
+        road_lengths_inverse.append(1/i.cost)
     Sum = sum(road_lengths_inverse)
 
     #I want sum of road_percentage to be 1.0
     roads_percentage = []
-    for i in range(len(road_lengths)):
-        roads_percentage.append(road_lengths_inverse.cost/Sum)
-    
+    for i in road_lengths_inverse:
+        roads_percentage.append(i/Sum)
+
     #Increasing diversity acording to selection_presure
     for i in range(selection_presure):
         for j in range(len(roads_percentage)):
@@ -26,28 +33,10 @@ def roulette(road_lengths, selection_presure):
     random_number = random.random()
     temp_number = 0
     outcome1 = 0
-    outcome2 = 0
 
     for i in range(len(roads_percentage)):
         temp_number += roads_percentage[i]
         if(temp_number > random_number):
             outcome1 = i
             break
-    
-    random_number = random.random()
-    temp_number = 0
-    for i in range(len(roads_percentage)):
-        temp_number += roads_percentage[i]
-        if(temp_number > random_number):
-            outcome2 = i
-            break
-    
-    return road_lengths[outcome1], road_lengths[outcome2]
-
-def tournament(population, selection_pressure):
-    contestants = random.sample(range(len(population)), selection_pressure)
-    winner = contestants[0]
-    for contestant in contestants[1:]:
-        if population[contestant] < population[winner]:
-            winner = contestant
-    return population[winner]
+    return population[outcome1]
